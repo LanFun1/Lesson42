29f9e851db75687603059d11210048103f73d22c
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-22 13:45:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int healthPercent, maxBar = 10, manaPercent;
+            Console.WriteLine("Введите оставшийся процент хп и маны персонажа");
+            healthPercent = Convert.ToInt32(Console.ReadLine());
+            manaPercent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите символ, которым будет обозначаться элемент здоровья");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.Clear();
+            DrawBar(healthPercent, maxBar, 0, ConsoleColor.Red, symbol);
+            DrawBar(manaPercent, maxBar, 1, ConsoleColor.Blue, symbol);
+        }
+
+        static void DrawBar(int percent, int maxBar, int position, ConsoleColor color, char symbol = ' ')
+        {
+            Console.SetCursorPosition(0, position);
+            Console.Write("[");
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            int health = (maxBar * percent) / 100;
+            for (int i = 0; i < maxBar; i++)
+            {
+                if (i < health)
+                    Console.Write(symbol);
+                else
+                    Console.Write(' ');
+            }
+            Console.ForegroundColor = defaultColor;
+            Console.Write("]");
+        }
+    }
+}
